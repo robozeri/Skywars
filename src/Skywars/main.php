acafeb54058d3be52aafaa3a9c65845cdca96b10
robozeri <robozerirobot@gmail.com>
2015-06-13 17:24:39 +0200

    Update main.php

--- src/Skywars/main.php
+++ src/Skywars/main.php
@@ -11,7 +11,7 @@ use pocketmine\command\CommandExecutor;
 class Main extends PluginBase implements CommandExecutor
 {
  public $commands;
- public $arenaManager;
+ public $worldManager;
  public $gameKit;
  public $setup;
  public $controller; 
@@ -19,7 +19,7 @@ class Main extends PluginBase implements CommandExecutor
   public function onLoad() 
   {
         $this->commands = new SkyWarsCommand ( $this );
-	$this->arenaManager = new SkyWarsArenaManager ( $this );
+	$this->worldManager = new SkyWarsWorldManager ( $this );
 	$this->gameKit = new SkyWarsGameKit($this);
 	$this->setup = new SKyWarsSetup($this);
 	$this->profileprovider = new SKyWarsProvider ( $this );
@@ -43,6 +43,28 @@ class Main extends PluginBase implements CommandExecutor
 		$this->log ( TextFormat::GREEN . "- SKyWars Enable -" );
   	
   }
+  private function initConfigFile() {
+		try {
+			if (! file_exists ( $this->getDataFolder () )) {
+				@mkdir ( $this->getDataFolder (), 0777, true );
+				file_put_contents ( $this->getDataFolder () . "config.yml", $this->getResource ( "config.yml" ) );
+			}
+			$this->saveDefaultConfig ();
+			$this->reloadConfig ();
+			$this->getConfig ()->getAll ();
+		} catch ( \Exception $e ) {
+			$this->getLogger ()->error ( $e->getMessage () );
+		}
+	}
+	private function initScheduler() {
+		// start count down to release them
+		$wait_time = 10 * $this->getServer ()->getTicksPerSecond ();
+		$arenaResetTask = new PlaySkyWarsWarsTask( $this );
+		$this->getServer ()->getScheduler ()->scheduleRepeatingTask ( $skywarsResetTask,60 );
+
+		$particleTask = new UpdatePlayerParticleTask($this);
+		$this->getServer ()->getScheduler ()->scheduleRepeatingTask ( $particleTask,30);
+	}
   public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
 		$this->commands->onCommand ( $sender, $command, $label, $args );
   public function onDisable
